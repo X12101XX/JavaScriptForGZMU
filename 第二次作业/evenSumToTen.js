a842028c5f771eebfd7fb8c0e5327fbfdc966874
X12101X <X12101XX@outlook.com>
2026-04-15 16:23:17 +0800

    第二次作业

--- 第二次作业/evenSumToTen.js
+++
@@ -1,6 +0,0 @@
-let result2 = 0;
-
-for(let i = 2; i <= 10; i += 2){
-    result2 += i;
-}
-document.getElementById("evenSumToTen").innerHTML = result2;
--- 第二次作业/fracToSeven.js
+++
@@ -1,6 +0,0 @@
-let result = 1
-for (let i = 1; i <= 7; ++i) {
-  result *= i;
-}
-document.getElementById("factorial").innerHTML = result
-
